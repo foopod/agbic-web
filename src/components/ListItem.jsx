9e515c88e01ab3088c4bc32f53febccfc6c1e076
Jono Shields <jonathonshields@gmail.com>
2023-06-26 18:56:14 +1200

    Add lazy loading for images

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,9 +3,11 @@ import './ListItem.css'
 
 function ListItem({cartridge, onClick}) {
 
+  const loading = cartridge.number > 8 ? 'lazy' : null;
+
   return (
     <div className='card' onClick={onClick}>
-        <img className='card-image' src={cartridge.image} alt={cartridge.name} />
+        <img className='card-image' src={cartridge.image} alt={cartridge.name} loading={loading}/>
         <div>
         {cartridge.isFav &&
           <FaHeart style={{color: 'hotpink'}}/>
